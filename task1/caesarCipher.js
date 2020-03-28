56f6f81a81ef5318f2ae646247d643ead0f0396c
Kr-Mx <maks.kryshnev@gmail.com>
2020-03-28 21:41:41 +0300

    feat: add caesar cipher encoding/decoding

---
+++ task1/caesarCipher.js
@@ -0,0 +1,37 @@
+const data = {
+  result: '',
+  minLowCharValue: 'a'.charCodeAt(0),
+  maxLowCharValue: 'z'.charCodeAt(0),
+  minUpCharValue: 'A'.charCodeAt(0),
+  maxUpCharValue: 'Z'.charCodeAt(0),
+  letterAmount: 26
+};
+
+function encode(text, shift) {
+
+  function transformedLetter(letter, correction) {
+    return String.fromCharCode(
+      ((letter + +shift - correction) % data.letterAmount) + correction
+    );
+  }
+
+  for (let i = 0; i < text.length; i++) {
+    let letter = text.charCodeAt(i);
+    if (letter >= data.minUpCharValue && letter <= data.maxLowCharValue) {
+      if (letter >= data.minUpCharValue && letter <= data.maxUpCharValue) {
+        data.result += transformedLetter(letter, data.minUpCharValue);
+      } else {
+        data.result += transformedLetter(letter, data.minLowCharValue);
+      }
+    } else {
+      data.result += text[i];
+    }
+  }
+  return data.result;
+}
+
+function decode(text, shift) {
+  return encode(text, (data.letterAmount - +shift) % data.letterAmount);
+}
+
+module.exports = { encode, decode };
