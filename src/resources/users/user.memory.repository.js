a19691200b0d79c427c84c5577206fd6abbb8b8f
Kr-Mx <maks.kryshnev@gmail.com>
2020-04-04 13:03:05 +0300

    feat: add users routes and data service

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,52 @@
+const User = require('./user.model');
+const users = [
+  {
+    id: '1',
+    name: 'Max',
+    login: 'Maxis',
+    password: 'Maxis11'
+  },
+  {
+    id: '2',
+    name: 'Mix',
+    login: 'Mixis',
+    password: 'Mixis11'
+  },
+  {
+    id: '3',
+    name: 'Mex',
+    login: 'Mexis',
+    password: 'Mexis11'
+  }
+];
+
 const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
+  return users;
+};
+const getUsersById = async (id) => {
+  return users.find((user) => {
+    return user.id === id;
+  });
+};
+const createUser = async (params) => {
+  const { login, name, password } = params;
+  const newUser = new User(login, name, password);
+  users.push(newUser);
+  return newUser;
+};
+const updateUser = async (id, data) =>{
+  const user = users.find(item => item.id === id);
+  Object.assign(user, data);
+  return user;
+};
+const deleteUser = async (id) =>{
+  const index = users.findIndex(user => user.id === id);
+ if(~index){
+   users.splice(index, 1);
+   return true;
+ }
+ else return false
 };
 
-module.exports = { getAll };
+
+module.exports = { getAll, getUsersById, createUser, updateUser, deleteUser };
